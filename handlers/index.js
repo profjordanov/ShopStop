d72047814c1e45f86edfbc6c350d6a788e374c8d
Jordan Jordanov <d_yugioh@abv.bg>
2017-10-08 21:47:19 +0300

    PartTreeFinished

--- handlers/index.js
+++ handlers/index.js
@@ -1,5 +1,6 @@
 const homeHandler = require('./home')
 const fileshandler = require('./static-files')
 const products = require('./product')
+const category = require('./category')
 
-module.exports = [homeHandler, fileshandler,products]
+module.exports = [homeHandler , fileshandler , products , category]
--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const http = require('http')
+const config = require('./config/config')
+const database = require('./config/database.config')
 const port = 3003
 const handlers = require('./handlers')
+
+let environment = process.env.NODE_environment || 'development'
+
+database(config[environment])
 
 http.createServer((req,res)=>{
   for(let handler of handlers){
