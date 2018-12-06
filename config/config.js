a124f6e946f092f015f8604c71561777978523cc
Jordan Jordanov <d_yugioh@abv.bg>
2018-12-06 19:57:50 +0200

    init

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ const path = require('path')
 
 module.exports = {
   development: {
-    connectionString: 'mongodb://localhost:27017/ShopStopDatabase',
+    connectionString: 'mongodb://shopstopdb:4Hay2T6GJwmmED634Jx0lmoBdZ2bcu1dUgxUlwQqsgxd2jfe7wUhiPBTjQQUhs4xqmhdqjaP7t9Wv2ZJqhA7Hw==@shopstopdb.documents.azure.com:10255/?ssl=true',
     rootPath: path.normalize(path.join(__dirname,'../'))
   },
   production: {
