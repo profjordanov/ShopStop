9060ac251d1bb22777c8df0a73f043e1648251e2
Jordan Jordanov <d_yugioh@abv.bg>
2018-12-08 07:20:37 +0200

    init

--- config/config.js
+++ config/config.js
@@ -6,6 +6,7 @@ module.exports = {
     rootPath: path.normalize(path.join(__dirname,'../'))
   },
   production: {
-
+    connectionString: 'mongodb://shopstopdb:4Hay2T6GJwmmED634Jx0lmoBdZ2bcu1dUgxUlwQqsgxd2jfe7wUhiPBTjQQUhs4xqmhdqjaP7t9Wv2ZJqhA7Hw==@shopstopdb.documents.azure.com:10255/?ssl=true',
+    rootPath: path.normalize(path.join(__dirname,'../'))
   }
 }
